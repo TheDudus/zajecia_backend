98aaa15ebc16bc871067610e2bb0eb75a979078b
bartosz.krochmal <thedudus2012@gmail.com>
2022-03-13 16:07:47 +0100

    Started ex 4 Started ex 5

---
+++ src/com/company/Main2.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.time.*;
+
+
+public class Main2 {
+
+    public static void main(String[] args){
+        System.out.println(java.time.LocalTime.now());
+
+        ZoneId global = ZoneId.of("Etc/Greenwich");
+
+        System.out.println(java.time.ZonedDateTime.now(global));
+    }
+}
+
+
---
+++ src/com/company/Main3.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main3 {
+
+    public static void main(String[] args) {
+        /*boolean[] CRC_TABLE = new boolean[256];
+
+        for( int i = 0; i < 256 ; i++ )
+        {
+            boolean code = (boolean) i;
+            for( int x = 0 ; x < 8 ; x++)
+            {
+                code = (code & 0x01 ? 0xEDB88320 ^ (code >>> 1) : (code >>> 1));
+            }
+            CRC_TABLE[i] = code;
+        }
+*/
+    }
+}
+
+
